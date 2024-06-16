b2c3637a5571cff12a1e555af902128a9a5582c6
Augustodalmas <augustodalmas132@gmail.com>
2024-06-16 15:41:31 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function scrollToSection(sectionId) {
+    document.getElementById(sectionId).scrollIntoView({ behavior: 'smooth' });
+}
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Mensagem enviada com sucesso!');
+    this.reset();
+});
